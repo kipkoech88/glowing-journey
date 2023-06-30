df7b5d1ecaa78b6907f8cb0118e635107e702018
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-30 22:50:09 +0300

    initial commit

---
+++ codingactuary/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi, I am Hosea Kipngetich</Text>
+      <Text>The Coding Actuary</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'cyan',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
